f9cccadd22e566e9531f2002425fd8600fa2c628
edgardoburgos94 <edgardo.burgos94@gmail.com>
2018-05-04 17:36:59 -0500

    Delete town-zone association if town status is inactive

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,4 +1,13 @@
 class Town < ApplicationRecord
   has_many :assignements, dependent: :destroy
   has_many :zones, through: :assignements
+
+  after_save :check_town_status
+
+  private
+    def check_town_status
+      if self.status == false
+        Assignement.where(town_id: self.id).delete_all
+      end
+    end
 end
